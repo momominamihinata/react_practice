659cc3377b0a6f1b03b2835c166af7d47c2592cd
momominami <k056112@kansai.u.ac.jp>
2024-02-25 20:03:00 +0900

    練習3

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,22 +1,20 @@
-const user = {
-  name: "Hedy Lamerr",
-  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
-  imageSize: 90,
-};
-
-export default function Profile() {
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={"Photo of " + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize,
-        }}
-      />
-    </>
-  );
+const products = [
+  {title:'Cabbage', isFruit: false, id:1},
+  {title:'Garlic', isFruit: false, id:2},
+  {title:'Apple', isFruit: true, id:3},
+];
+export default function ShoppingList(){
+  const listItems = products.map(product =>
+    <li
+     key={product.id}
+     style={{
+      color:product.isFruit ? 'magenta' : 'darkgreen'
+     }}
+    >
+      {product.title}
+    </li>
+    );
+    return(
+      <ul>{listItems}</ul>
+    );
 }
