552a946153ce5680b8efb98b34b8a3c5a2edb9e3
momominami <k056112@kansai.u.ac.jp>
2024-02-25 17:30:22 +0900

    練習2

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,14 +1,22 @@
-function MyButton(){
-  return (
-    <button>I'm a button</button>
-  );
-}
+const user = {
+  name: "Hedy Lamerr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
 
-export default function MyApp(){
-  return(
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </>
   );
 }
